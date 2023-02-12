a306f6848c2d9c9471c802489da82fad44b27725
Azfar <azfarmehmood998@gmail.com>
2023-02-12 15:07:47 +0100

    Game loop

---
+++ app/src/main/java/com/example/androidgame2d/Game.java
@@ -0,0 +1,74 @@
+package com.example.androidgame2d;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Game manages all objects in the game and is responsible for updating and rendering
+ * all objects on the screen
+ */
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private GameLoop gameLoop;
+    private Context context;
+
+    public Game(Context context) {
+        super(context);
+
+        // Get surface holder and add callback
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        this.context = context;
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS " + averageUPS, 100, 100, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS " + averageFPS, 100, 200, paint);
+    }
+
+    public void update() {
+    }
+}
--- app/src/main/java/com/example/androidgame2d/MainActivity.java
+++ app/src/main/java/com/example/androidgame2d/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.androidgame2d;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set window to full screen to hide the status bar
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game view so that the objects in the game can be rendered to the screen
+        setContentView(new Game(this));
     }
 }
